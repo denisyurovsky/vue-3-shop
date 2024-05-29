4f468782409cbf7b49904d90d7033f94b8048a02
denisyurovsky <moon.wave578@gmail.com>
2024-05-29 12:59:22 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 export default  {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  media: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
